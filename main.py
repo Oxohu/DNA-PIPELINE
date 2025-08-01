300534e28093f2326c9c6e4d79dcc8411ece5fb7
Oxohu <Ozohu.15@gmail.com>
2025-08-01 14:28:40 +0100

    COUNT

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 
-fasta_file = 'sequences.fasta'
-for record in SeqIO.parse(fasta_file , "fasta"):
-    print(f"ID: {record.id}")
-    print(f"Description: {record.description}")
-    print(f"Sequence Length: {len(record.seq)}")
-    print(f"Sequence: {record.seq[:50]}...")  # Print first 50 characters of the sequence
-    print("-" * 40)
+fasta_file = 'sequence.fasta'
+for record in SeqIO.parse(fasta_file, "fasta"):
+    print("🔹 Gene ID:", record.id)
+    print("🔹 Description:", record.description)
+    print("🔹 Sequence Length:", len(record.seq))
+    print("🔹 First 100 bases:\n", record.seq[:100])
+
+def count_bases(sequence):
+    #Count the occurrences of each base in the sequence
+    base_counts = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+    for base in sequence:
+        if base in base_counts:
+            base_counts[base] += 1
+    return base_counts
+count_bases = count_bases(record.seq)
+print("🔹 Base Counts:", count_bases)
+
+def count_pairs(sequence):
+    #to count the occurences of each pair
+    pair_counts = {'AC': 0, 'GC' : 0, 'TA' : 0, 'GA' : 0 }
+    JK
