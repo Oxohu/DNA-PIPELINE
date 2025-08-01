8f632cbe09e6c2aeda170248873f980782aa2a7a
Oxohu <Ozohu.15@gmail.com>
2025-08-01 08:16:12 +0100

    count base sequences

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Bio import SeqIO
 import matplotlib.pyplot as plt
 
-
-def read_fasta(file_path):
-    '''Reads a FASTA file and returns a list of sequences.'''
+fasta_file = 'sequences.fasta'
+for record in SeqIO.parse(fasta_file , "fasta"):
+    print(f"ID: {record.id}")
+    print(f"Description: {record.description}")
+    print(f"Sequence Length: {len(record.seq)}")
+    print(f"Sequence: {record.seq[:50]}...")  # Print first 50 characters of the sequence
+    print("-" * 40)
